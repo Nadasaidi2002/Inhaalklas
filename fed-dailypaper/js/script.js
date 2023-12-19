c28df845c443223bd7aef08d4aa420ca8b929789
nada saidi <118190251+Nadasaidi2002@users.noreply.github.com>
2023-12-19 18:49:16 +0100

    versie 2 Daily paper

---
+++ fed-dailypaper/js/script.js
@@ -0,0 +1,95 @@
+
+
+derdep=document.querySelector("li:nth-of-type(3) p");
+
+/* stap 2 */
+/* laat deDerdeP luisteren naar kliks */
+/* en voer dan de functie veranderMezelf uit */
+derdep.onclick=ietsdoen;
+
+
+/* stap 3 */
+/* maak de functie veranderMezelf aan */
+/* voeg in de functie de class anders toe aan deDerdeP */
+
+function ietsdoen(){
+  // alert('jup')
+  derdep.classList.add("anders")
+}
+
+
+
+
+/*******************************/
+/* CODE VOOR HET VIERDE BLOKJE */
+/*******************************/
+
+/* stap 1 */
+/* zoek de p in de vierde li op */
+/* sla die op in de const deVierdeP */
+/* zoek de button in de vierde li op */
+/* sla die op in de const deVierdeButton */
+
+
+/* stap 2 */
+/* laat de deVierdeButton luisteren naar kliks */
+/* en voer dan de functie veranderDeVierdeP uit */
+
+
+/* stap 3 */
+/* maak de functie veranderDeVierdeP aan */
+/* toggle in de functie de class anders op/af deVierdeP */
+
+
+
+
+
+/******************************/
+/* CODE VOOR HET VIJFDE BLOKJE */
+/******************************/
+
+/* jouw code hier */
+
+
+
+
+
+/******************************/
+/* CODE VOOR HET ZESDE BLOKJE */
+/******************************/
+
+/* jouw code hier */
+
+
+
+
+
+/********************************/
+/* CODE VOOR HET ZEVENDE BLOKJE */
+/********************************/
+
+/* jouw code hier */
+
+
+
+
+
+/********************************/
+/* CODE VOOR HET ACHTSTE BLOKJE */
+/********************************/
+
+/* jouw code hier */
+
+
+
+
+
+/********************************/
+/* CODE VOOR HET NEGENDE BLOKJE */
+/********************************/
+
+/* jouw code hier */
+
+
+
+
